c73efd6d41a0f698cc1d0d9626a8e958bab86a4e
tryknowledge - CLehue <clehue@ast.cl>
2025-08-05 12:18:55 -0400

    Update chat message handling to include 'bot-bienvenida' sender and modify initial bot message; adjust InfoModal content for clarity and style improvements.

--- src/views/Analisis/components/InfoModal.tsx
+++ src/views/Analisis/components/InfoModal.tsx
@@ -1,82 +1,6 @@
 import { X } from "lucide-react";
 
 export const InfoModal = ({ onClose: onClose }: { onClose: () => void }) => {
-  const infoContent = (
-    <>
-      <div className="mb-2">
-        <h3 className="text-xl font-semibold mb-3">
-          ¡Hola! Soy WIS-AI <span className="text-red-500">WIS-AI</span>
-        </h3>
-        <p className="text-gray-400 leading-relaxed">
-          Bienvenido a la sección de Análisis, una interfaz diseñada para
-          ofrecer acceso a datos específicos y detallados organizados por
-          centros. Actualmente disponemos de información de los centros PIRQUEN
-          y POLOCUHE. A modo de demostración, la plataforma permite consultar
-          tres bases de datos principales: Clima, Alimentación e Informes
-          Ambientales (este último exclusivo para Pirquen).
-        </p>
-      </div>
-
-      <div className="mb-2">
-        <h4 className="font-bold text-lg mb-3 text-[#b6d6f0] border-b pb-1">
-          1. Datos Climáticos
-        </h4>
-        <p className="mb-2 text-gray-400">
-          Información meteorológica que incluye temperatura, velocidad del
-          viento y presión atmosférica.
-        </p>
-        <p className="text-sm italic text-gray-500 mb-3">
-          Ejemplos de consulta: Puede solicitar datos de estas variables dentro
-          de rangos temporales específicos.
-        </p>
-        <ul className="list-disc list-inside ml-4 mb-3 space-y-1 text-sm text-gray-400">
-          <li>
-            "Muestra un gráfico de registros de temperatura del 1 de junio para
-            Pirquen"
-          </li>
-          <li>"¿Cuál fue la temperatura el 3 de mayo en pirquen?"</li>
-          <li>
-            ¿me puedes dar un grafico de clima y alimentacion para pirquen?
-          </li>
-        </ul>
-      </div>
-
-      <div className="mb-2">
-        <h4 className="font-bold text-lg mb-3 text-[#b6d6f0] border-b pb-1">
-          2. Sistema de Alimentación
-        </h4>
-        <p className="mb-2 text-gray-400">
-          Datos detallados sobre el estado de los sistemas de alimentación en
-          ambos centros.
-        </p>
-        <p className="text-sm italic text-gray-500 mb-3">
-          Ejemplos de consulta: Puede indagar sobre el estado del suministro y
-          posibles incidencias.
-        </p>
-        <ul className="list-disc list-inside ml-4 mb-3 space-y-1 text-sm text-gray-400">
-          <li>"muestrame datos de alimentación para pirquen"</li>
-        </ul>
-      </div>
-
-      <div className="mb-2">
-        <h4 className="font-bold text-lg mb-3 text-[#b6d6f0] border-b pb-1">
-          3. Informes Ambientales
-        </h4>
-        <p className="mb-2 text-gray-400">
-          Resúmenes de datos ambientales del fondo marino (actualmente
-          disponible solo para Pirquen).
-        </p>
-        <p className="text-sm italic text-gray-500 mb-3">
-          Ejemplos de consulta: Solicite informes para evaluar condiciones del
-          fondo marino, especialmente útiles para cultivos.
-        </p>
-        <ul className="list-disc list-inside ml-4 mb-3 space-y-1 text-sm text-gray-400">
-          <li>"Muestra el último informe ambiental de Pirquen"</li>
-          <li>Dame datos del último informe comparativo de pirquen</li>
-        </ul>
-      </div>
-    </>
-  );
 
   type InfoModalProps = {
     children?: React.ReactNode;
@@ -85,11 +9,33 @@ export const InfoModal = ({ onClose: onClose }: { onClose: () => void }) => {
   };
 
   const CardStyles = ({ children, title, subtitle }: InfoModalProps) => {
+    const colorSelect = (title: string) => {
+      if (title.includes("Datos")) {
+        return "#1dddff";
+      } else if (title.includes("Sistema")) {
+        return "#ffcd16";
+      } else {
+        return "#bbf451";
+      }
+    };
+
     return (
-      <div className="relative hover:scale-102 cursor-pointer hover:mx-2 transition-all duration-300 bg-gradient-to-b from-[#04040a] to-[#0dcac7] h-160 w-100 rounded-2xl p-[1px] shadow-xl shadow-amber-400/10">
-        <div className="relative bg-[#04040a] opacity-90 overflow-hidden   h-full w-full rounded-2xl">
+      <div className="relative  cursor-pointer hover:mx-2 transition-all duration-300 bg-gradient-to-b from-[#04040a] to-[#0dcac7] h-160 w-100 rounded-2xl p-[1px] shadow-xl shadow-amber-400/10">
+        <div className="relative bg-[#04090a] opacity-90 overflow-hidden   h-full w-full rounded-2xl">
+          <div className="absolute -bottom-20 left-[50%] -translate-x-1/2 h-20 w-50 rounded-full bg-lime-500 blur-3xl opacity-15"></div>
+          <div
+            style={{
+              backgroundColor: colorSelect(title),
+            }}
+            className="absolute -bottom-13 left-[50%] -translate-x-1/2 h-30 w-30 rounded-full bg-sky-500 blur-2xl opacity-15"
+          ></div>
           <div className="relative h-10 w-full flex justify-end items-center px-4 border-b border-b-[#29293f]">
-            <h2 className="absolute left-[50%] -translate-x-1/2 text-sky-400 text-nowrap text-center text-lg">
+            <h2
+              className="absolute left-[50%]  -translate-x-1/2 text-sky-400 text-nowrap text-center text-lg"
+              style={{
+                color: colorSelect(title),
+              }}
+            >
               {title}
             </h2>
             <button
@@ -100,7 +46,7 @@ export const InfoModal = ({ onClose: onClose }: { onClose: () => void }) => {
             </button>
           </div>
           <div className="w-full flex flex-col justify-center items-center ">
-            <h3 className="text-gray-400 text-xs">{subtitle}</h3>
+            <h3 className="text-gray-400 text-xs py-5">{subtitle}</h3>
           </div>
           <div className=" w-full h-full py-2 px-5">{children}</div>
         </div>
@@ -115,7 +61,7 @@ export const InfoModal = ({ onClose: onClose }: { onClose: () => void }) => {
           ¡Te damos la bienvenida a la sección de Análisis!
         </p>
         <p className="text-balance text-center text-amber-500 mb-4">
-          (Versión beta)
+          (Versión beta) Beta 1.0.2
         </p>
         <p className="text-balance text-center text-gray-300">
           Actualmente, puedes acceder a información de los centros{"  "}
@@ -149,24 +95,29 @@ export const InfoModal = ({ onClose: onClose }: { onClose: () => void }) => {
             subtitle="Rango de 2025-05-03 a 2025-07-14"
             title="Datos Climáticos"
           >
-            <div className="mb-2 w-full">
-              <p className="mb-2 text-gray-100 text-center ">
+            <div className="mb-2 w-full max-w-[300px] mx-auto">
+              <p className="mb-5 text-gray-100 text-center font-bold ">
                 Información meteorológica que incluye temperatura, velocidad del
                 viento y presión atmosférica.
               </p>
-              <span className="text-center"> Ejemplos de consulta</span>
-              <p className="text-xs italic text-gray-400 mb-3 text-center">
-                , Puedes solicitar datos de estas variables dentro de rangos
+              <p className="text-center bg-sky-700/20 rounded mb-2 py-0.5 text-white">
+                {" "}
+                Ejemplos de consulta
+              </p>
+              <p className="text-xs italic text-gray-400 mb-6 text-center ">
+                Puedes solicitar datos de estas variables dentro de rangos
                 temporales específicos.
               </p>
-              <ul className="list-disc list-inside ml-4 mb-3 space-y-1 text-sm text-gray-400">
-                <li>
-                  "Muestra un gráfico de registros de temperatura del 1 de junio
-                  para Pirquen"
+              <ul className="mb-3 space-y-0.5 text-sm text-white">
+                <li className="hover:bg-sky-950 border-s border-s-sky-400 ps-3 min-h-16 border-b border-neutral-400/40 py-3">
+                  Muestra un gráfico de registros de temperatura del 1 de junio
+                  para Pirquen
                 </li>
-                <li>"¿Cuál fue la temperatura el 3 de mayo en pirquen?"</li>
-                <li>
-                  ¿me puedes dar un grafico de clima y alimentacion para
+                <li className="hover:bg-sky-950 border-s border-s-sky-400 ps-3 min-h-16 border-b border-neutral-400/40 py-3">
+                  ¿Cuál fue la temperatura el 3 de mayo en pirquen?
+                </li>
+                <li className="hover:bg-sky-950 border-s border-s-sky-400 ps-3 min-h-16 border-b border-neutral-400/40 py-3">
+                  ¿Me puedes dar un grafico de clima y alimentacion para
                   pirquen?
                 </li>
               </ul>
@@ -176,13 +127,62 @@ export const InfoModal = ({ onClose: onClose }: { onClose: () => void }) => {
             subtitle="Rango de 2024-09-01 a 2025-06-24"
             title=" Sistema de Alimentación"
           >
-            <h1>Prueba</h1>
+            <div className="mb-2 w-full max-w-[300px] mx-auto">
+              <p className="mb-5 text-gray-100 text-center font-bold ">
+                Datos detallados sobre el estado de los sistemas de alimentación
+                en ambos centros.
+              </p>
+              <p className="text-center bg-amber-400/20 rounded mb-2 py-0.5 text-white">
+                {" "}
+                Ejemplos de consulta
+              </p>
+              <p className="text-xs italic text-gray-400 mb-6 text-center ">
+                Puedes solicitar datos de estas variables dentro de rangos
+                temporales específicos.
+              </p>
+              <ul className="mb-3 space-y-0.5 text-sm text-white">
+                <li className="hover:bg-amber-950 border-s border-s-amber-400 ps-3 min-h-16 border-b border-neutral-400/40 py-3">
+                  Muestrame datos de alimentación para pirquen
+                </li>
+                <li className="hover:bg-amber-950 border-s border-s-amber-400 ps-3 min-h-16 border-b border-neutral-400/40 py-3">
+                  Generame un gráfico de alimentación para pirquen
+                </li>
+                <li className="hover:bg-amber-950 border-s border-s-amber-400 ps-3 min-h-16 border-b border-neutral-400/40 py-3">
+                  Entrégame un análisis de los últimos datos de alimentación de
+                  pirquén
+                </li>
+              </ul>
+            </div>
           </CardStyles>
           <CardStyles
             subtitle="Rango de 8 informes del 2023 al 2024"
             title="Informes Ambientales"
           >
-            <h1>Prueba</h1>
+            <div className="mb-2 w-full max-w-[300px] mx-auto">
+              <p className="mb-5 text-gray-100 text-center font-bold ">
+                Resúmenes de datos ambientales del fondo marino (actualmente
+                disponible solo para Pirquen).
+              </p>
+              <p className="text-center bg-lime-700/20 rounded mb-2 py-0.5 text-white">
+                {" "}
+                Ejemplos de consulta
+              </p>
+              <p className="text-xs italic text-gray-400 mb-6 text-center ">
+                Puedes solicitar datos de estas variables dentro de rangos
+                temporales específicos.
+              </p>
+              <ul className="mb-3 space-y-0.5 text-sm text-white">
+                <li className="hover:bg-lime-950 border-s border-s-lime-400 ps-3 min-h-16 border-b border-neutral-400/40 py-3">
+                  Muestra el último informe ambiental de Pirquen
+                </li>
+                <li className="hover:bg-lime-950 border-s border-s-lime-400 ps-3 min-h-16 border-b border-neutral-400/40 py-3">
+                  ¿Dame datos del último informe comparativo de pirquen?
+                </li>
+                <li className="hover:bg-lime-950 border-s border-s-lime-400 ps-3 min-h-16 border-b border-neutral-400/40 py-3">
+                  ¿Según el último informe consideras que es un ambiente óptimo para el crecimiento de salmones, en pirquen?
+                </li>
+              </ul>
+            </div>
           </CardStyles>
         </div>
       }
